dc16e22712a3d8c00a071a23368fb5b0130e2436
andy decandia <andy@mesacloud.com>
2023-10-17 11:45:59 +0200

    fix action generation and random nav in prod

--- src/app/helper/openai.ts
+++ src/app/helper/openai.ts
@@ -1,11 +1,44 @@
 import chalk from "chalk";
 import logger from "@/app/helper/logger";
 
-const API_URL = 'https://api.openai.com/v1/chat/completions';
+import OpenAI from 'openai';
+// import { OpenAIStream,  } from 'ai';
+// import { OpenAIStream, StreamingTextResponse } from 'ai';
+
 const API_KEY = process.env.REACT_APP_OPENAI_KEY
-const HEADERS = {
-    "Content-Type": "application/json",
-    'Authorization': `Bearer ${API_KEY}`
+
+async function POST(messages: any, model: string) {
+    const openai = new OpenAI({
+        apiKey: process.env.REACT_APP_OPENAI_KEY,
+    });
+
+    const response = await openai.chat.completions.create({
+        model: model,
+        stream: false,
+        messages: messages
+    });
+
+    return response
+}
+
+const API_URL = 'https://api.openai.com/v1/chat/completions';
+
+async function POST_FETCH(messages: any, model: string, temperature?: string) {
+    return await fetch(API_URL, {
+        cache: 'no-store',
+        method: "POST",
+        body: JSON.stringify({
+            model: model,
+            messages: messages,
+            temperature: temperature,
+            max_tokens: 1000
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${API_KEY}`
+        },
+    }).then(res => res.json())
+
 }
 
 class Chat {
@@ -13,6 +46,9 @@ class Chat {
     stack: { role: string, content: string }[] = [];
     tokens: number
     timer: Date
+    // model: string = "gpt-3.5-turbo" // price: .0004 / 1K
+    // model: string = "gpt-3.5-turbo-0301"
+    model: string = "gpt-3.5-turbo-0613" // fastest
 
     constructor() {
         this.tokens = 0
@@ -21,7 +57,7 @@ class Chat {
 
     last = () => this.stack[this.stack.length - 1].content;
 
-    async send({message, temperature = 0.9, task = null }: {message: string, temperature?: number, task?: string | null }): Promise<string> {
+    async send({ message, temperature = 0.9, task = null }: { message: string, temperature?: number, task?: string | null }): Promise<string> {
         const newMessage = { "role": "user", "content": message.concat() };
         this.stack.push(newMessage);
         await this.fetch(this.stack, temperature, task)
@@ -29,35 +65,31 @@ class Chat {
     }
 
     async fetch(messages: { role: string, content: string }[], temperature: number, task: string | null,) {
-        if (task!=null) console.debug('OpenAI Fetch Started: ' + task.toString())
-        const response = await fetch(API_URL, {
-            cache: 'no-store',
-            method: "POST",
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo", // price: .002 / 1K
-                // model: "babbage-002", // price: .0004 / 1K
-                messages: messages,
-                temperature: temperature,
-                max_tokens: 1000
-            }),
-            headers: HEADERS,
-        }).then(res => res.json())
-        if (task!=null) console.debug('OpenAI Fetch Finished: ' + task.toString())
+        // if (task != null) console.debug('OpenAI Fetch Started: ' + task.toString())
+        // const response: any = await POST_FETCH(messages, this.model)
+        const response: any = await POST(messages, this.model)
+        // if (task != null) console.debug('OpenAI Fetch Finished: ' + task.toString())
         if (response.error) {
             console.error(`GPT_ERROR: ${response.error.type} - ${response.error.message} \n`);
             throw new Error('Model Request Error: ' + response.error.message)
         }
-        const newMessage = response.choices[0].message
-        this.tokens += response.usage.total_tokens
-        this.stack.push(newMessage)
+        else if (response.choices[0].finish_reason != 'stop') {
+            console.error(`GPT_INCOMPLETE: ${response.choices[0].finish_reason} \n`);
+            throw new Error('OpenAI Response Incomplete: ' + response.choices[0].finish_reason)
+        }
+        else {
+            const newMessage = response.choices[0].message
+            this.tokens += response.usage.total_tokens
+            this.stack.push(newMessage)
+        }
     }
 
     logAPIMetrics = (task?: string) => {
         const taskMessage = !task ? '' : ` (${task})`
         const timer = ` ${(((new Date()).getTime() - this.timer.getTime()) / 1000).toFixed(1)} seconds`
-        if (this.tokens <= 200) console.info(chalk.bold.greenBright('Tokens Used: ' + this.tokens) + taskMessage + timer + '\n');
-        else if (this.tokens <= 500) console.info(chalk.bold.redBright('Tokens Used: ' + this.tokens) + taskMessage + timer + '\n');
-        else { console.info(chalk.bold.bgRedBright('Tokens Used: ' + this.tokens) + taskMessage + timer + '\n'); }
+        if (this.tokens <= 200) console.info(chalk.bold.greenBright('Tokens Used: ' + this.tokens) + taskMessage + timer + ` (${this.model})` + '\n');
+        else if (this.tokens <= 500) console.info(chalk.bold.redBright('Tokens Used: ' + this.tokens) + taskMessage + timer + ` (${this.model})` + '\n');
+        else { console.info(chalk.bold.bgRedBright('Tokens Used: ' + this.tokens) + taskMessage + timer + ` (${this.model})` + '\n'); }
     }
 
 }
@@ -67,7 +99,7 @@ export class ChatGPTClient {
     static async generateDescription(): Promise<string> {
         const descriptionMessage = "Write a very random very short description about for a fictional story in 2nd person narrative. Any random theme and audience. Two sentences and 20 words maximum."
         const chat = new Chat()
-        const segment = await chat.send({message: descriptionMessage, temperature: 1.1 })
+        const segment = await chat.send({ message: descriptionMessage, temperature: 1.1 })
         chat.logAPIMetrics('description')
 
         return segment
@@ -78,8 +110,8 @@ export class ChatGPTClient {
         const tagMessage = "generate a tag for this story which contains 7 lowercase words with hyphens in between"
 
         const chat = new Chat()
-        const title = await chat.send({message: titleMesage})
-        const tag = await chat.send({message: tagMessage})
+        const title = await chat.send({ message: titleMesage })
+        const tag = await chat.send({ message: tagMessage })
         chat.logAPIMetrics('title and tag')
 
         return [title.replaceAll('"', ''), tag]
@@ -89,7 +121,7 @@ export class ChatGPTClient {
         const segmentMessage = "Write a beggining of a story in 2nd person narrative that eloborates on the following story: \n\n" + description + " \n\nThe output should be about 40 words long and the time frame of the story events should be immediate."
 
         const chat = new Chat()
-        const segment = await chat.send({message: segmentMessage})
+        const segment = await chat.send({ message: segmentMessage })
         chat.logAPIMetrics('introduction')
 
         return segment
@@ -103,9 +135,9 @@ export class ChatGPTClient {
 
         const choices = []
         const chat = new Chat()
-        choices.push(await chat.send({message: initialChoiceMessage, task:'choice'}))
+        choices.push(await chat.send({ message: initialChoiceMessage, task: 'choice' }))
         for (let i = 1; i < numChoices; i++) {
-            choices.push(await chat.send({message: additionalChoiceMessage, task:'next choice'}))
+            choices.push(await chat.send({ message: additionalChoiceMessage, task: 'next choice' }))
         }
         chat.logAPIMetrics('choices')
 
@@ -122,7 +154,7 @@ export class ChatGPTClient {
         const nextSegmentMessage = story + "\n\n" + "Given the following action, please provide one paragraph of about 30 words which describes the next segment that continues this story:" + "\n\n" + choice
 
         const chat = new Chat()
-        const nextSegment = await chat.send({message: nextSegmentMessage, task:'segment'})
+        const nextSegment = await chat.send({ message: nextSegmentMessage, task: 'segment' })
         chat.logAPIMetrics('segment')
         const nextChoices = await this.generateChoices([...segments, nextSegment], numChoices)
 
@@ -134,7 +166,7 @@ export class ChatGPTClient {
         const endingMessage = story + "\n\n" + "Given the following action, please provide one paragraph of about 30 words which describes an ending to this story:" + "\n\n" + choice
 
         const chat = new Chat()
-        const ending = await chat.send({message: endingMessage, task:'ending'})
+        const ending = await chat.send({ message: endingMessage, task: 'ending' })
         chat.logAPIMetrics('ending')
 
         return ending
